bd55fda9412b1736e1f319c5647469ab991eb7b0
Alam Ferrera <alamferrera@gmail.com>
2021-12-13 15:30:00 -0400

    reiniciar added
    funcionalidad de reiniciar agregada

--- Basket Timer/app.js
+++ Basket Timer/app.js
@@ -8,57 +8,68 @@ const btnReiniciar = document.getElementById('btnReiniciar');
 let id;
 
 btnIniciar.addEventListener('click', () => {
-    if(btnIniciar.classList.contains("iniciar")){
+    if (btnIniciar.classList.contains("iniciar")) {
         Pausa();
-    }
-    else{
+    } else {
         Reanudar();
     }
 });
 
-function Pausa(){
-    btnIniciar.value = "Reanudar";
-    btnIniciar.classList.remove("iniciar");
-    clearInterval(id);
+btnReiniciar.addEventListener('click', () => {
+    valueIzq.value = "00";
+    valueDer.value = "00";
+    min.value = "00";
+    seg.value = "00";
+});
+
+function Pausa() {
+    if ((min.value < 10 && min.value <= 0) && (seg.value < 10 && seg.value <= 0)) {
+        return false;
+    } else {
+        btnIniciar.value = "Reanudar";
+        btnIniciar.classList.remove("iniciar");
+        clearInterval(id);
+    }
 }
 
-function Reanudar(){
-    btnIniciar.value = "Pausa";
-    btnIniciar.classList.add("iniciar");
-    iniciarConteo();
+function Reanudar() {
+    if ((min.value < 10 && min.value <= 0) && (seg.value < 10 && seg.value <= 0)) {
+        return false;
+    } else {
+        btnIniciar.value = "Pausa";
+        btnIniciar.classList.add("iniciar");
+        iniciarConteo();
+    }
 }
 
 function iniciarConteo() {
-    if( (min.value < 10 && min.value <= 0) && (seg.value < 10 && seg.value <= 0)){
+    if ((min.value < 10 && min.value <= 0) && (seg.value < 10 && seg.value <= 0)) {
         return false;
-    }
-    else{
+    } else {
         id = setInterval(function () {
-            if(seg.value > 0){
+            if (seg.value > 0) {
                 seg.value--;
             }
-            if(seg.value < 10 && seg.value > 0){
-                seg.value = "0"+seg.value;
-            }
-            else{
+            if (seg.value < 10 && seg.value > 0) {
+                seg.value = "0" + seg.value;
+            } else {
                 seg.value = "00";
                 btnIniciar.value = "Iniciar";
             }
-            if(seg.value <= 0){
-                if(min.value <= 0){
+            if (seg.value <= 0) {
+                if (min.value <= 0) {
                     return false;
-                }
-                else{
-                    if(min.value < 10 && min.value > 0){
-                        min.value = "0"+min.value;
+                } else {
+                    if (min.value < 10 && min.value > 0) {
+                        min.value = "0" + min.value;
                     }
                     seg.value = "59";
                 }
             }
-            if(seg.value <= 0){
+            if (seg.value <= 0) {
                 clearInterval(id);
             }
         }, 1000);
     }
-    
+
 }
