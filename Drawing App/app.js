18daf2ef659f96ffceed2a3446fdd5b92c118dce
Alam Ferrera <alamferrera@gmail.com>
2021-12-15 13:13:25 -0400

    draw app advanced
    avance de la app

--- Drawing App/app.js
+++ Drawing App/app.js
@@ -1,21 +1,85 @@
 const canvas = document.getElementById("canvas");
 const ctx = canvas.getContext("2d");
+const increase = document.getElementById("increase");
+const decrease = document.getElementById("decrease");
+const clear = document.getElementById("clear");
+const sizeLabel = document.getElementById("size");
+const colorEl = document.getElementById("color");
+const clearEl = document.getElementById("clear");
 
-let size = 20;
+let size = 5;
+sizeLabel.innerText = size;
 let x = undefined;
 let y = undefined;
 let isPressed = false;
+let color = "";
+
+clearEl.addEventListener("click", () => {
+    console.log("clickeado");
+});
+
+colorEl.addEventListener("change", (e) => {
+    color = colorEl.value;
+});
+
+increase.addEventListener("click", ()=> {
+    size += 5;
+    
+    if(size >= 50){
+        size = 50;
+    }
+    sizeLabel.innerText = size;
+});
+
+decrease.addEventListener("click", ()=> {
+    size -= 5;
+    
+    if(size <= 5){
+        size = 5;
+    }
+    sizeLabel.innerText = size;
+});
 
 canvas.addEventListener("mousedown", (e) => {
     isPressed = true;
     x = e.offsetX;
     y = e.offsetY;
+
+    drawCircle(x,y);
 });
+
+canvas.addEventListener("mouseup", (e) => {
+    isPressed = false;
+    x = undefined;
+    y = undefined;
+});
+
+canvas.addEventListener("mousemove", (e) => {
+    if(isPressed){
+       const x2 = e.offsetX;
+       const y2 = e.offsetY;
+       drawCircle(x2,y2);
+       drawLine(x,y,x2,y2);
+
+       x = x2;
+       y = y2;
+    }
+});
+
+function drawLine(x1,y1, x2, y2){
+    ctx.beginPath();
+    ctx.moveTo(x1,y1);
+    ctx.lineTo(x2,y2);
+    ctx.strokeStyle = color;
+    ctx.lineWidth = size * 2;
+    ctx.stroke();
+}
 
 function drawCircle(x, y){
     ctx.beginPath();
     ctx.arc(x, y, size, 0, Math.PI * 2);
-    ctx.stroke();
+    ctx.fillStyle = color;
+    ctx.fill();
 }
 
-drawCircle(50, 50);
+
