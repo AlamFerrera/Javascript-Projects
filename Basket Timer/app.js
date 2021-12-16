c2b8c5a28a6fa67f9f184d838e7805c610adda10
Alam Ferrera <alamferrera@gmail.com>
2021-12-16 11:35:58 -0400

    updates
    updates

--- Basket Timer/app.js
+++ Basket Timer/app.js
@@ -23,44 +23,40 @@ btnIniciar.addEventListener('click', () => {
 
 upValueIzq.addEventListener("click", () => {
     newValueIzq++;
-    
-    if(newValueIzq < 10){
-        valueIzq.innerText = "0"+newValueIzq;
-    }
-    else{
+
+    if (newValueIzq < 10) {
+        valueIzq.innerText = "0" + newValueIzq;
+    } else {
         valueIzq.innerText = newValueIzq;
     }
 });
 
 downValueIzq.addEventListener("click", () => {
     newValueIzq > 0 ? newValueIzq-- : "";
-    
-    if(newValueIzq < 10){
-        valueIzq.innerText = "0"+newValueIzq;
-    }
-    else{
+
+    if (newValueIzq < 10) {
+        valueIzq.innerText = "0" + newValueIzq;
+    } else {
         valueIzq.innerText = newValueIzq;
     }
 });
 
 upValueDer.addEventListener("click", () => {
     newValueDer++;
-    
-    if(newValueDer < 10){
-        valueDer.innerText = "0"+newValueDer;
-    }
-    else{
+
+    if (newValueDer < 10) {
+        valueDer.innerText = "0" + newValueDer;
+    } else {
         valueDer.innerText = newValueDer;
     }
 });
 
 downValueDer.addEventListener("click", () => {
     newValueDer > 0 ? newValueDer-- : "";
-    
-    if(newValueDer < 10){
-        valueDer.innerText = "0"+newValueDer;
-    }
-    else{
+
+    if (newValueDer < 10) {
+        valueDer.innerText = "0" + newValueDer;
+    } else {
         valueDer.innerText = newValueDer;
     }
 });
@@ -69,12 +65,17 @@ btnReiniciar.addEventListener('click', () => {
     Reiniciar();
 });
 
-function Reiniciar(){
+function Reiniciar() {
     valueIzq.innerText = "00";
     valueDer.innerText = "00";
     min.value = "00";
     seg.value = "00";
     btnIniciar.value = "Iniciar";
+    min.classList.remove("end");
+    seg.classList.remove("end");
+    btnIniciar.classList.remove("iniciar");
+    min.removeAttribute("disabled");
+    seg.removeAttribute("disabled");
     clearInterval(id);
 }
 
@@ -83,6 +84,8 @@ function Pausa() {
         return false;
     } else {
         btnIniciar.value = "Reanudar";
+        min.removeAttribute("disabled");
+        seg.removeAttribute("disabled");
         btnIniciar.classList.remove("iniciar");
         clearInterval(id);
     }
@@ -92,6 +95,10 @@ function Reanudar() {
     if ((min.value < 10 && min.value <= 0) && (seg.value < 10 && seg.value <= 0)) {
         return false;
     } else {
+        min.setAttribute("disabled", "disabled");
+        seg.setAttribute("disabled", "disabled");
+        min.style.color = "#000";
+        seg.style.color = "#000";
         btnIniciar.value = "Pausa";
         btnIniciar.classList.add("iniciar");
         iniciarConteo();
@@ -108,7 +115,7 @@ function iniciarConteo() {
             }
             if (seg.value > 0 && seg.value < 10) {
                 seg.value = "0" + seg.value;
-            } else if(seg.value <= 0) {
+            } else if (seg.value <= 0) {
                 seg.value = "00";
             }
 
@@ -117,14 +124,15 @@ function iniciarConteo() {
                     min.value--;
                     min.value = "0" + min.value;
                     seg.value = "59";
-                }
-                else if(min.value > 0){
+                } else if (min.value > 0) {
                     min.value--;
                     seg.value = 59;
                 }
             }
-            
+
             if (seg.value <= 0) {
+                min.classList.add("end");
+                seg.classList.add("end");
                 clearInterval(id);
             }
         }, 1000);
